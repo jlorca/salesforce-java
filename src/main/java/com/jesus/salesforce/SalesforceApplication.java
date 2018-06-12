424dc059402eb50ecd88a24c8ed536471f9ae000
Jesus Lorca Luque <jeslorlu@gmail.com>
2018-06-12 21:59:20 +0200

    Initial commit

---
+++ src/main/java/com/jesus/salesforce/SalesforceApplication.java
@@ -0,0 +1,12 @@
+package com.jesus.salesforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesforceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesforceApplication.class, args);
+	}
+}
