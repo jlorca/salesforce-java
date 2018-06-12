d8501cf3a472c4c32dbf5ae471d7eccb794fc654
Jesus Lorca Luque <jeslorlu@gmail.com>
2018-06-12 22:02:41 +0200

    add config and init method

--- src/main/java/com/jesus/salesforce/SalesforceApplication.java
+++ src/main/java/com/jesus/salesforce/SalesforceApplication.java
@@ -1,12 +1,26 @@
 package com.jesus.salesforce;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class SalesforceApplication {
+
+	@Value("${amazonProperties.endpointUrl}")
+	private String endpointUrl;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalesforceApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "HI HI HI!";
 	}
 }
