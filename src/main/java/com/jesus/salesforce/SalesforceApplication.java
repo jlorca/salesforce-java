811a322bc66fd9e1dc6e4464853e6983c29c3082
Jesus Lorca Luque <jeslorlu@gmail.com>
2018-06-13 10:49:40 +0200

    first rest call from java

--- src/main/java/com/jesus/salesforce/SalesforceApplication.java
+++ src/main/java/com/jesus/salesforce/SalesforceApplication.java
@@ -1,5 +1,7 @@
 package com.jesus.salesforce;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +9,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+
 @SpringBootApplication
 @Controller
 public class SalesforceApplication {
 
-	@Value("${amazonProperties.endpointUrl}")
-	private String endpointUrl;
+	@Value("${amazonProperties.environment}")
+	private String environment;
+
+	@Value("${amazonProperties.clientId}")
+	private String clientId;
+
+	@Value("${amazonProperties.clientSecret}")
+	private String clientSecret;
+
+	@Value("${amazonProperties.redirectUri}")
+	private String redirectUri;
+
+	@Value("${amazonProperties.restUrl}")
+	private String restUrl;
 
 	@RequestMapping("/")
 	@ResponseBody
@@ -23,7 +39,25 @@ public class SalesforceApplication {
 	@RequestMapping("/jesus")
 	@ResponseBody
 	String other() {
-		return "HI HI HI!";
+		HttpClient httpclient = new HttpClient();
+		GetMethod getMethod = new GetMethod(restUrl);
+		getMethod.setRequestHeader("Authorization", "OAuth " + clientId);
+//		getMethod.addParameter("grant_type","authorization_code");
+//
+//		// For session ID instead of OAuth 2.0, use "grant_type", "password"
+//		getMethod.addParameter("client_id",clientId);
+//		getMethod.addParameter("client_secret",clientSecret);
+//		getMethod.addParameter("redirect_uri",redirectUri);
+
+		String responseBody = null;
+		try {
+			httpclient.executeMethod(getMethod);
+			responseBody = getMethod.getResponseBodyAsString();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return responseBody;
 	}
 
 	public static void main(String[] args) {
